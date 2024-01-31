97b8cb2f5a185e742505d183b86ae61d27fbff68
Nhung Nguyen <sofpast@gmail.com>
2024-01-31 23:07:36 +0700

    custom

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -2,12 +2,27 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import initialize_agent, Tool, AgentType
 
+
 def lookup(name: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     template = """given the full name {name_of_person} I want you
     to get it me a link to their Linkedin profile page.
     Your answer should contain only URL"""
 
-    tools_for_agent = [Tool(name="Crawl Google 4 linkedin profile page"), func="?", description="useful when you need to get" ]
-    agent = 
+    tools_for_agent = [Tool(
+        name="Crawl Google 4 linkedin profile page",
+        func="?",
+        description="useful when you need get the Linkedin page URL",
+    )
+    ]
+    agent = initialize_agent(tools=tools_for_agent,
+                             llm=llm,
+                             agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    prompt_template = PromptTemplate(
+        template=template,
+        input_variables=['name_of_person']
+    )
+
+    linked_profile_url = agent.run(prompt_template.format_prompt(name_of_person=name))
+    
     return "Linkedin Profile URL"
